34e8b32fae34cb5a67956646e8e8bfb5525823b7
pardhiv kumar chippada <159048635+Pardhivkumar@users.noreply.github.com>
2024-08-04 15:36:03 +0530

    codeforces HQ9+ solution

---
+++ HQ9+.py
@@ -0,0 +1,11 @@
+str=input("")
+if("H" in str):
+    print("YES")
+elif("Q" in str)   :
+    print("YES")
+elif("9" in str)   :
+    print("YES")
+elif("+" in str)   :
+    print("NO")
+else:
+    print("NO")
